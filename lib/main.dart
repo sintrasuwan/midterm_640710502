278bb9c657afaac2041a679ad549b8c550496c76
sintrasuwan <sintrasuwan@gmail.com>
2024-01-26 21:52:35 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const midterm(),
+    );
+  }
+}
+
+class midterm extends StatefulWidget {
+  const midterm({super.key});
+
+  @override
+  State<midterm> createState() => _midtermState();
+}
+
+class _midtermState extends State<midterm> {
+
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
